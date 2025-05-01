9ef3f6e952e1c47454b1aef98a30bca40e9dcbf7
ChristianLaraa <christian.lara.091204@gmail.com>
2025-05-01 14:13:11 -0600

    add java bytecodes

---
+++ javaByteCodes/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,19 @@
+package javaapplication2;
+
+/**
+ *
+ * @author chris
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         for (int i = 0; 1 < 65; i++){
+           System.out.println(i);
+       }
+    }
+    
+}
---
+++ javaByteCodes/JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,19 @@
+package javaapplication3;
+
+/**
+ *
+ * @author chris
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int a, b,c;
+       a = 3;
+       b = 4;
+       c = a*b;
+       System.out.println(c);
+    }  
+}
